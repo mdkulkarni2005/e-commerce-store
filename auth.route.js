0b42106efcf534876eda9086d74fd62ce50a7edf
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-09-18 20:05:28 +0530

    the final website code

--- auth.route.js
+++ auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { login, logout, signup, refreshToken, getProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);  // POST request for signup
-router.post("/login", login);    // POST request for login
-router.post("/logout", logout);  // POST request for logout
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/refresh-token", refreshToken);
+router.get("/profile", protectRoute, getProfile);
 
 export default router;
--- redis.js
+++ redis.js
@@ -4,3 +4,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+
+
